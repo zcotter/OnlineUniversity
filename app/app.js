777db91c0caec30074b9f46cdaa0cb006ce4c7c4
Zach Cotter <cotter.z@husky.neu.edu>
2015-03-28 17:17:18 -0400

    app no styling or functions

--- app/app.js
+++ app/app.js
@@ -1,12 +1,16 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('onlineUniversity', [
-  'ngRoute',
-  'onlineUniversity.view1',
-  'onlineUniversity.view2',
-  'onlineUniversity.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+var app = angular.module('onlineUniversity', []);
+
+app.controller("OnlineUniversityController", function ($scope)
+{
+  $scope.courses = [ { name : "Java 101", category : "PROG", dateCreated : "1/1/2015", description : "Wow" },
+  { name : "MongoDB 101", category : "DB", dateCreated : "2/1/2015", description : "Good" },
+  { name : "Express 101", category : "PROG", dateCreated : "3/1/2015", description : "Better" },
+  { name : "AngularJS 101", category : "WEB", dateCreated : "4/1/2015", description : "Best" },
+  { name : "NodeJS 101", category : "PROG", dateCreated : "5/1/2015", description : "Awesome" } ];
+});
+//config(['$routeProvider', function($routeProvider) {
+  //$routeProvider.otherwise({redirectTo: '/view1'});
+//}]);
